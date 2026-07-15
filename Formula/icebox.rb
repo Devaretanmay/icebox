e5891657c8bea846cce15a289a98cd273a16a583
devaretanmay <devaretanmay@users.noreply.github.com>
2026-07-16 01:17:33 +0530

    chore: setup distribution, policy packs, k8s manifests, and homebrew

---
+++ Formula/icebox.rb
@@ -0,0 +1,17 @@
+class Icebox < Formula
+  desc "Runtime governance for autonomous offensive security"
+  homepage "https://github.com/Devaretanmay/icebox"
+  url "https://github.com/Devaretanmay/icebox/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/icebox", "--version"
+  end
+end
